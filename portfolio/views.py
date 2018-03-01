dc77d571268505cb5fdcba31fc4a0533bfaab9a8
tutordelphia <adamlevin@tutordelphia.com>
2018-03-01 17:40:29 -0500

    updates portfolio to work with newest version of grade-it

--- portfolio/views.py
+++ portfolio/views.py
@@ -2,43 +2,57 @@ from django.shortcuts import render
 from django.http import HttpResponseRedirect
 import sys
 import os
+import random
 
 from .forms import WorksheetForm
 
-sys.path.append(os.path.abspath('../grade-it'))
-from worksheet_gen import WorksheetGen
-#from talk.models import Post
-#from talk.forms import PostForm
+sys.path.append(os.path.abspath('../grade-it/grade_it'))
+from worksheet_gen import generate_worksheet, PDF
 
+def random_hex():
+    return str(random.choice("0123456789ABCDEF"))
+def get_random_hex(size=10):
+    result = "".join([random_hex() for _ in range(size)])
+    return result
 
 def worksheet_form(request):
     if request.method == 'POST':
         form = WorksheetForm(request.POST)
-
         if form.is_valid():
             min_val = form.cleaned_data['min_val']
             max_val = form.cleaned_data['max_val']
             problem_count = form.cleaned_data['problem_count']
             qr = bool(form.cleaned_data.get('qr', False))
 
-            specs = {
-                "problem_type": "add",
-                "problem_count": problem_count,
-                "min_val" : min_val,
-                "max_val" : max_val,
-                "qr" : qr,
-                "folder" : "static/sheets"
-            }
-            my_gen = WorksheetGen("add", "add", specs)
+            worksheet_specs = {
+                "name": "add",
+                "qr": qr, 
+                "folder" : "static/sheets",
+                "qr_type": "answer_key", # or "form_code"
+                "random_seed": get_random_hex(),
+                "problem_groups": [
+                    {
+                        "count": problem_count,
+                        "group_name": "basicOp",
+                        "specs": {
+                            "type": "add",
+                            "randomize": True,
+                            "min_value": min_val,
+                            "max_value": max_val
 
-            sheet = my_gen.create_sheet()
-            sheet = sheet.replace("portfolio", "")
+                        }
+                    }
+                ]
+            }
+
+    
+            sheet = generate_worksheet(worksheet_specs, display_method=PDF)
+
             view_data = {
                 'result': sheet
             }
 
             return render(request, 'grade-it/worksheet.html', view_data)
-
     else:
         form = WorksheetForm()
     
@@ -64,6 +78,7 @@ def addTwo(request):
     
     return render(request, 'grade-it/worksheet.html', view_data)
 
+'''
 def createWorksheet(request):
     # http://127.0.0.1:8000/static/sheets/add_2018-01-23_17-40-47.pdf
 
@@ -90,3 +105,4 @@ def createWorksheet(request):
     }
 
     return render(request, 'grade-it/worksheet.html', view_data)
+'''
